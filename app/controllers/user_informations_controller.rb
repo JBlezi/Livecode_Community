aaa0939f1c0eba2f795232c8989f4f99633eff17
Luciano Petillo <luciano.prnl@gmail.com>
2022-05-31 17:33:20 +0200

    redirecting to tickets after complete profile

--- app/controllers/user_informations_controller.rb
+++ app/controllers/user_informations_controller.rb
@@ -12,7 +12,7 @@ class UserInformationsController < ApplicationController
     def update
       @user_information = UserInformation.find(params[:id])
       @user_information.update(user_information_params)
-      redirect_to new_user_session_path
+      redirect_to  tickets_path
       authorize @user_information
     end
 
