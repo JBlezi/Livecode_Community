ad906db6bb0ccb45b28fc71f3f4ab649142d32e7
Chloe Cartron <chloe.cartron@gmail.com>
2022-05-31 17:53:33 +0200

    test the video chat integration

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,15 +7,5 @@ import { application } from "./application"
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
-// Uncomment this line if you want to use the Stimulus Twilio Video controller
-//   without inheriting it:
-// import TwilioVideoController from 'stimulus-twilio-video'
-
-const application = Application.start()
-const context = require.context("controllers", true, /_controller\.js$/)
-// Uncomment this line if you want to use the Stimulus Twilio Video controller
-//   without inheriting it:
-// application.register('video-call', TwilioVideoController)
-application.load(definitionsFromContext(context))
+import VideoCallController from "./video_call_controller"
+application.register("video-call", VideoCallController)
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,12 @@ module.exports = {
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 1
-    })
+    }),
+    new webpack.ProvidePlugin({
+      // Make a global `process` variable that points to the `process` package,
+      // because the `util` package expects there to be a global variable named `process`.
+           // Thanks to https://stackoverflow.com/a/65018686/14239942
+      process: 'process/browser'
+   })
   ]
 }
