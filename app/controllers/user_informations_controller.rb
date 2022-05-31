1f37ac88000721833cb0ddbcf8a28280092fdf3d
Luciano Petillo <luciano.prnl@gmail.com>
2022-05-31 17:13:00 +0200

    redirect to new user after fill user information form

--- app/controllers/user_informations_controller.rb
+++ app/controllers/user_informations_controller.rb
@@ -10,7 +10,15 @@ class UserInformationsController < ApplicationController
     end
 
     def update
-        @user_information = UserInformation.find(params[:id])
-        authorize @user_information
+      @user_information = UserInformation.find(params[:id])
+      @user_information.update(user_information_params)
+      redirect_to home_path
+      authorize @user_information
+    end
+
+    private
+
+    def user_information_params
+     params.require(:user_information).permit(:first_name, :last_name, :bio, :github_url)
     end
 end
