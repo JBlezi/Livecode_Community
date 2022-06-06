edcf08cd9193c191b775f1371bc0c2574c36417e
Chloe Cartron <chloe.cartron@gmail.com>
2022-06-06 17:56:17 +0200

    screen sharing

--- app/javascript/controllers/video_call_controller.js
+++ app/javascript/controllers/video_call_controller.js
@@ -1,38 +1,62 @@
 import TwilioVideoController from 'stimulus-twilio-video'
 
+import { connect, createLocalVideoTrack } from 'twilio-video'
+
 export default class extends TwilioVideoController {
   static targets = ['noCall', 'awaitingBuddy', 'joinCallButton', 'endCallButton', 'screenShareButton', 'endScreenShareButton']
 
-  screenShare() {
-    console.log("COUCOU")
-  }
 
-  endShare() {
-    console.log("lalalalala")
-  }
+  shareScreenHandler() {
+    let screenTrack
 
-  callStarted() {
-    this.noCallTarget.classList.add('d-none')
-    this.awaitingBuddyTarget.classList.remove('d-none')
-    this.joinCallButtonTarget.classList.add('d-none')
-    this.endCallButtonTarget.classList.remove('d-none')
-  }
+    console.log('click on share screen')
 
-  callEnded() {
-    console.log('Call ended!')
-    this.noCallTarget.classList.remove('d-none')
-    this.awaitingBuddyTarget.classList.add('d-none')
-    this.joinCallButtonTarget.classList.remove('d-none')
-    this.endCallButtonTarget.classList.add('d-none')
-  }
+    event.preventDefault();
 
-  buddyJoined() {
-    console.log('Buddy has joined')
-    this.awaitingBuddyTarget.classList.add('d-none')
-  }
+    if (!screenTrack) {
+        navigator.mediaDevices.getDisplayMedia().then(stream => {
 
-  buddyLeft() {
-    console.log('Buddy has left')
-  }
+          screenTrack = createLocalVideoTrack(stream.getTracks()[0]);
+            console.log(screenTrack);
+            room_id.localParticipant.publishTrack(screenTrack);
+            console.log(screenTrack)
+            this.innerHTML = 'Stop sharing';
+            screenTrack.mediaStreamTrack.onended = () => { shareScreenHandler() };
+        })
+        // .catch(() => {
+        //     alert('Could not share the screen.');
+        // });
+    }
+    else {
+        room_id.localParticipant.unpublishTrack(screenTrack);
+        screenTrack.stop();
+        screenTrack = null;
+        this.innerHTML = 'Share screen';
+    }
 
-}
+  };
+};
+
+// callStarted() {
+  //   this.noCallTarget.classList.add('d-none')
+  //   this.awaitingBuddyTarget.classList.remove('d-none')
+  //   this.joinCallButtonTarget.classList.add('d-none')
+  //   this.endCallButtonTarget.classList.remove('d-none')
+  // }
+
+  // callEnded() {
+  //   console.log('Call ended!')
+  //   this.noCallTarget.classList.remove('d-none')
+  //   this.awaitingBuddyTarget.classList.add('d-none')
+  //   this.joinCallButtonTarget.classList.remove('d-none')
+  //   this.endCallButtonTarget.classList.add('d-none')
+  // }
+
+  // buddyJoined() {
+  //   console.log('Buddy has joined')
+  //   this.awaitingBuddyTarget.classList.add('d-none')
+  // }
+
+  // buddyLeft() {
+  //   console.log('Buddy has left')
+  // }
