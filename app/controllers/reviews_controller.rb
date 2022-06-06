1289101f73ad53fe85aa3c6c4954e98699f914e0
geza steinert <gwasteinert@gmail.com>
2022-06-06 18:07:05 +0200

    default

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,16 @@
 class ReviewsController < ApplicationController
   before_action :set_ticket, only: [:new, :create]
 
+  def index
+    policy_scope(Review)
+    @review = Review.new
+    @ticket = Ticket.find(params[:carehome_id])
+  end
+
+  def show
+    @ratings = {score: @tickets.reviews.average(:rating).to_f.round(1)}
+  end
+
   def new
     # We need @ticket in our `simple_form_for`
     @review = Review.new
